fb9ddd7e32f8c73724d4107be5d23b09ae20a253
Phaneendhra <phani4842@gmail.com>
2023-04-11 23:46:00 +0530

    React Learning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Axios from "axios";
 
 function App() {
 
@@ -11,14 +12,17 @@ function App() {
   const Names = ["Toxic","Light","Ryuk","Reyna","Sova","BrimStone","Raze","Cypher"] // this is a list
 
   const [showText,setShowText] = useState(true);
-  
+  const [catFact,setCatFact]  = useState("");
   useEffect(()=>{ 
     /*
       built-in hook in React that allows functional components to perform side effects, such as fetching data from a server, updating the DOM, or subscribing to a WebSocket.
       Very useful if we are updating a component, fetching data from server updating a hook (useState)
       */
-    console.log("Function Triggered")   
-  },[showText]);
+    Axios.get("https://catfact.ninja/fact").then((res)=>{
+      setCatFact(res.data.fact);          // for every time the showText is set we call this API using Axiom
+    })
+    console.log("Function Triggered")
+  },[showText]);            
 
   return (
     <div className="App">
@@ -37,6 +41,8 @@ function App() {
       setShowText(!showText)
     }}> Display/Hide Name</button>
     {showText && <h3>User Name : {dict.Name}</h3>}
+
+    <p>{catFact}</p>  {/*This is set for every time showText Change */}
     </div>
   );
 }
